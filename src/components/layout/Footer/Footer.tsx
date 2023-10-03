e6736d44295d1447c1c181d40ba35586fea37ecc
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-10-03 12:55:40 -0300

    [feat] add footer component

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Logo from "@/assets/logo.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="h-[7.5rem] w-full flex items-center justify-between">
+      <p>2023 - present | Miguel Barbosa</p>
+      <Image
+        style={{ userSelect: "none" }}
+        draggable={false}
+        src={Logo}
+        alt="m1guelsb-logo"
+        height={40}
+      />
+    </footer>
+  );
+};
