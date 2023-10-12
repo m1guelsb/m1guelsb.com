82c983a44189c7d1e849a6ae357822f2302bae8d
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-10-11 22:44:16 -0300

    [style] add layout animations to article list

--- src/components/layout/blog/ArticlesList.tsx
+++ src/components/layout/blog/ArticlesList.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
 import { ArticleCard } from "@/components/ui";
 import { Article, PaginationResponse } from "@/types";
 
@@ -14,16 +16,22 @@ export const ArticlesList = ({ articlesData }: ArticlesListProps) => {
         </span>
       </div>
 
-      <div className="grid grid-cols-2 gap-[1rem]">
-        {articlesData?.content.map(({ id, title, brief, categories }) => (
-          <ArticleCard
-            key={id}
-            title={title}
-            brief={brief}
-            categories={categories}
-          />
+      <motion.div className="grid grid-cols-2 gap-[1rem]">
+        {articlesData?.content.map(({ ...article }) => (
+          <AnimatePresence mode="popLayout" key={article.id}>
+            <motion.div
+              key={article.id}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              style={{ zIndex: `${article.id! + 1}` }}
+            >
+              <ArticleCard {...article} />
+            </motion.div>
+          </AnimatePresence>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
