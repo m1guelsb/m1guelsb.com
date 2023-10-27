55bcf0990319b8f3a4a8a867c7379f2cc4b7d7a8
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-10-26 22:46:10 -0300

    [article] add handling-spring-exceptions article

--- src/mdx-database/articles/index.ts
+++ src/mdx-database/articles/index.ts
@@ -7,7 +7,10 @@ export const articles: Article[] = [
     title: "Erros comuns de iniciantes em ReactJS",
     brief:
       "Neste artigo revisamos as pegadinhas mais comuns do React, por que acontecem e como resolve-las. 🧐",
-    categories: [categories[0], categories[1]],
+    categories: [
+      categories.find((c) => c.title === "Javascript")!,
+      categories.find((c) => c.title === "ReactJS")!,
+    ],
     topics: [
       "Propriedade style no JSX",
       "Acessar um valor que ainda não existe",
@@ -17,5 +20,25 @@ export const articles: Article[] = [
     body: `react-beginner-mistakes.mdx`,
     createdAt: "2023-10-18",
     updatedAt: "2023-10-18",
+  },
+  {
+    id: 2,
+    title: "Como tratar erros no spring boot",
+    brief:
+      "Veremos o jeito mais fácil de lidar com erros no Spring-boot, retornando mensagens formatadas e claras para o client.",
+    categories: [
+      categories.find((c) => c.title === "Java")!,
+      categories.find((c) => c.title === "Spring-boot")!,
+      categories.find((c) => c.title === "Tutorial")!,
+    ],
+    topics: [
+      "Propriedade style no JSX",
+      "Acessar um valor que ainda não existe",
+      "A propriedade key quando exibindo uma lista",
+      "Verificando se uma lista está vazia",
+    ],
+    body: `handling-spring-exceptions.mdx`,
+    createdAt: "2023-10-27",
+    updatedAt: "2023-10-27",
   },
 ];
