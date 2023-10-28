221b08bf644b757e43dd361a3885144d3ccb3e79
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-10-28 00:52:24 -0300

    [fix] adjust formatDate func

--- src/utils/functions/formatDate.ts
+++ src/utils/functions/formatDate.ts
@@ -1,2 +1,5 @@
 export const formatDate = (date: string) =>
-  new Date(date).toLocaleDateString("pt-br");
+  new Date(date).toLocaleDateString("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    dateStyle: "medium",
+  });
